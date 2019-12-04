696544a24ca716f01af21fcd0e46d4cf9cba9917
Daniël Terwiel <daniel.terwiel@sendcloud.com>
2019-12-04 19:57:11 +0100

    update

--- script.js
+++ script.js
@@ -22,3 +22,19 @@ function validateForm(event) {
     event.preventDefault();
   }
 }
+
+const roleRadios = document.querySelectorAll('[name="role"]')
+roleRadios.forEach(roleRadio => {
+  roleRadio.addEventListener('change', event => {
+    const submitButton = document.querySelector('#submit')
+    const id = event.target.id
+    if (id === 'recruiter') {
+      submitButton.classList.add('form__submit-button--fade-out')
+      submitButton.classList.remove('form__submit-button--fade-in')
+    } else {
+      submitButton.classList.add('form__submit-button--fade-in')
+      submitButton.classList.remove('form__submit-button--fade-out')
+    }
+  })
+})
+
