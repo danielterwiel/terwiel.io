4f38493ae266c40dc1c14e21c5e0890d5e4239bd
Daniël Terwiel <daniel.terwiel@sendcloud.com>
2019-12-11 22:00:15 +0100

    inlined svgs

--- script.js
+++ script.js
@@ -1,28 +1,3 @@
-function validateForm(event) {
-  event.preventDefault();
-
-  debugger
-
-  // TODO: this does not work
-
-  alert();
-  var name = document.form.contact.name.value;
-  var email = document.form.contact.email.value;
-  var role = document.form.contact.role.value;
-  var message = document.form.contact.message.value;
-
-  if (role === "recruiter") {
-    event.preventDefault();
-    alert(
-      "My apologies, nothing personal, but I'd rather pro-actively look for a job myself"
-    );
-  }
-
-  if (!name || !email || !message) {
-    event.preventDefault();
-  }
-}
-
 const roleRadios = document.querySelectorAll('[name="role"]')
 roleRadios.forEach(roleRadio => {
   roleRadio.addEventListener('change', event => {
@@ -37,4 +12,3 @@ roleRadios.forEach(roleRadio => {
     }
   })
 })
-
