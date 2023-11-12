cdfe0184823d44bc3b68bdb4cd0ce403f6c9b329
Daniël Terwiel <daniel@terwiel.io>
2023-11-12 16:51:58 +0100

    let next handle favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,11 @@ const inter = Inter({
 export const metadata = {
   title: "Terwiel - Freelance Developer",
   description:
-    "An accomplished Frontend-end developer with a focus on communication, architecture, performance, accessibility",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+    "An accomplished developer with a focus on the web, architecture, performance, accessibility",
+  visualViewport: {
+    name: "theme-color",
+    content: "#d9723f",
+  },
 };
 
 export default function RootLayout({
@@ -21,89 +24,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-        <title>Daniël Terwiel - Front-End Developer</title>
-        <meta
-          name="description"
-          content="An accomplished Frontend-end developer with a focus on communication, architecture, performance, accessibility"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="57x57"
-          href="/apple-icon-57x57.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="60x60"
-          href="/apple-icon-60x60.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="72x72"
-          href="/apple-icon-72x72.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="76x76"
-          href="/apple-icon-76x76.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="114x114"
-          href="/apple-icon-114x114.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="120x120"
-          href="/apple-icon-120x120.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="144x144"
-          href="/apple-icon-144x144.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/apple-icon-152x152.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-icon-180x180.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="192x192"
-          href="/android-icon-192x192.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="96x96"
-          href="/favicon-96x96.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.webmanifest" />
-        <meta name="msapplication-TileColor" content="#d9723f" />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#d9723f" />
-      </head>
       <body className={`font-sans ${inter.variable}`}>{children}</body>
     </html>
   );
