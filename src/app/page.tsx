bea13b7ca3d0cc78b7595c7d69616787e9d5c766
Daniël Terwiel <daniel@terwiel.io>
2024-01-08 15:07:08 +0100

    style: remove page footer & header on print

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "~/components/footer";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col">
+    <main className="flex min-h-screen flex-col print:m-0">
       <div>
         <Header />
         <About />
