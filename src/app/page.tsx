6828181482c26e0cdb681d53414130523e1ebe99
Daniël Terwiel <daniel@terwiel.io>
2025-10-08 21:29:10 +0200

    chore: increase spacing between search input and cloud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { StackCloud } from "~/components/stack-cloud";
 export default function HomePage() {
   return (
     <div className="flex justify-center">
-      <main className="flex min-h-screen max-w-xl p-4 flex-col print:m-0 a4-page prose w-full">
+      <main className="flex min-h-screen max-w-xl p-4 flex-col print:m-0 a4-page prose w-full gap-6">
         <SearchInput />
         <StackCloud />
         <Experience />
